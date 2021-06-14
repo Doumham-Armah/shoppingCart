4a7c720702b94975b13193f10b700ba6e71475d2
mahmoudhamra <mahmoudhamra@gmail.com>
2021-06-14 15:55:52 +0300

    added buttons to increment/decrement items

--- src/Cart.js
+++ src/Cart.js
@@ -4,7 +4,22 @@ import CartItem from './CartItem'
 // looping over the items
 function Cart({ initialItems }){
 
-    const GrandTotal = initialItems.reduce((total, item) => (
+
+    const [items, setItems] = useState(initialItems)
+
+    // update the qty
+    const updateQty = (id, newQty) => {
+        const newItems = items.map(item => {
+          if (item.id === id) {
+            return { ...item, qty: newQty };
+          }
+          return item;
+        });
+        setItems(newItems);
+      };
+
+    // calcs the grand total of all items in the cart
+    const GrandTotal = items.reduce((total, item) => (
         total + item.qty * item.price), 0).toFixed(2)
 
 
@@ -12,8 +27,8 @@ function Cart({ initialItems }){
         <div className="Cart">
           <h1 className="Cart-title">Shopping Cart</h1>
           <div className="Cart-items">
-            {initialItems.map(item => (
-              <CartItem {...item} key={item.id} />
+            {items.map(item => (
+              <CartItem {...item} updateQty={updateQty} key={item.id} />
             ))}
           </div>
           <h2>Grand total: {GrandTotal}</h2>
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
-function CartItem({id, itemName, price, qty}){
+function CartItem({id, itemName, price, qty, updateQty }){
+
+    // call updateQty function
+    const addOne = () => updateQty(id, qty + 1)
+    const subtractOne = () => updateQty(id, qty - 1)
+
 
     return(
         
         <div className="CartItem">
             <div>{itemName}</div>
             <div>${price}</div>
-            <div>{qty}</div>
+            <div>
+                <button onClick={subtractOne} disabled={qty<=1}>-</button>
+                {qty}
+                <button onClick={addOne}disabled={qty>=100}>+</button>
+            </div>
             <div>{`Total : ${qty*price}`}</div>
 
 
