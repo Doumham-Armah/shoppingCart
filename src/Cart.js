8b95b87e623b022a28b6a8d93db81981d510c354
mahmoudhamra <mahmoudhamra@gmail.com>
2021-06-14 14:02:07 +0300

    added grand total for Cart component

--- src/Cart.js
+++ src/Cart.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 import CartItem from './CartItem'
 
-
+// looping over the items
 function Cart({ initialItems }){
 
-    
+    const GrandTotal = initialItems.reduce((total, item) => (
+        total + item.qty * item.price), 0).toFixed(2)
+
+
     return (
         <div className="Cart">
           <h1 className="Cart-title">Shopping Cart</h1>
@@ -13,6 +16,7 @@ function Cart({ initialItems }){
               <CartItem {...item} key={item.id} />
             ))}
           </div>
+          <h2>Grand total: {GrandTotal}</h2>
         </div>
       );
 }
